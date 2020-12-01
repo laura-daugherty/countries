bccdcc08b2434c6c0517d0e07510e57426b38b94
Laura Daugherty <lauradaugherty6@gmail.com>
2020-11-30 17:09:13 -0800

    changing from country param to alpha3code

--- rest-countries-api-with-color-theme-switcher-master/countries/src/components/countryPage.tsx
+++ rest-countries-api-with-color-theme-switcher-master/countries/src/components/countryPage.tsx
@@ -2,29 +2,33 @@ import { useEffect, useState } from 'react'
 import {Country} from './countriesList'
 import React from 'react';
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
 
 type Props = {
-  match: { params: { name: any; }; }
+  match: { params: { 
+    alpha3Code: any;
+  }; }
 }
 
 const CountryPage = (props: Props) => {
   const countryNull: Country | null = null;
+
   const [country, setCountry] = useState(countryNull)
 
   useEffect(() => {
     console.log("props.match", props.match)
-    const name = props.match.params.name;
+    const alpha3Code = props.match.params.alpha3Code;
     Axios
-      .get(`https://restcountries.eu/rest/v2/name/${name}`)
+      .get(`https://restcountries.eu/rest/v2/alpha/${alpha3Code}`)
       .then(response => {
         console.log("response", response)
-        setCountry(response.data[0])
+        setCountry(response.data)
         console.log("country", country)
       })
       .catch(error => {
         console.log(error)
       })
-  }, [])
+  }, [props.match.params.alpha3Code])
 
   
  function isCountry(country: Country | null) {
@@ -34,9 +38,32 @@ const CountryPage = (props: Props) => {
         <img src={country.flag}/>
         <div className="countryCardInfo">
           <h2> {country.name}</h2>
+          <p>Native Name: {country.nativeName}</p>
           <p>Population: {country.population}</p>
           <p>Region: {country.region}</p>
+          <p>Sub Region: {country.subregion}</p>
           <p>Capital: {country.capital}</p>
+          <p>Top Level Domain: {country.topLevelDomain}</p>
+          <p>Currencies: {country.currencies.map(currency => {
+            return (
+              <div>
+                {currency.name}
+              </div>
+            )
+          })}</p>
+          <p>Languages: {country.languages.map(language => {
+            return (
+              <div>
+                {language.name}, 
+              </div>)
+          })}</p>
+          <p>Border Countries: {country.borders.map(border => {
+            return (
+              <Link to={`/countries/${border}`}>{border}</Link>
+            )
+          })}
+
+          </p>
         </div>    
       </div>
     )
@@ -56,4 +83,5 @@ const CountryPage = (props: Props) => {
   )
 
 };
+
 export default CountryPage;
