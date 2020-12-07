ba1c6914c30eeea91a83a8c9acbd19c6c9d977b2
Laura Daugherty <lauradaugherty6@gmail.com>
2020-12-07 15:13:28 -0800

    app darkmode

--- rest-countries-api-with-color-theme-switcher-master/countries/src/App.tsx
+++ rest-countries-api-with-color-theme-switcher-master/countries/src/App.tsx
@@ -9,10 +9,19 @@ import CountryPage from './components/countryPage';
 function App() {
   const [isDarkmode, setIsDarkmode] = useState(false)
 
+  const darkmode = () => {
+    if (isDarkmode) {
+      document.body.classList.add("dark")
+    } else {
+      document.body.classList.remove("dark")
+    }
+  }
+
   return (
     <div className="App">
+      {darkmode()}
       <Header isDarkmode={isDarkmode} setIsDarkmode={setIsDarkmode}/>
-      <Route exact path="/" render={() => <CountriesList/>}/>
+      <Route exact path="/" render={(props) => <CountriesList {...props} isDarkmode={isDarkmode}/>}/>
       <Route
           path="/countries/:alpha3Code"
           render={(props) => <CountryPage {...props}/>}
